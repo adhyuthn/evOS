64bf4e94fc66700ffc683a260799f4b891ce9818
adhyuthn <adhyuthnarayan008@gmail.com>
2024-08-21 22:03:49 +0530

    ✓ stage - 12 Multiprogramming

--- xfs_inputs.sh
+++ xfs_inputs.sh
@@ -1,10 +1,10 @@
-# The .sh is given for the sake of syntax highlighting :)
 fdisk
 # Stage 11 specifies to load lib before bootstrap. Not sure why.
-load --library /home/evoprime/Athena/PROJ/evOS/stages/stage_11/assign_write_library.lib
-load --os /home/evoprime/Athena/PROJ/evOS/stages/stage_11/os_startup.xsm
-load --init /home/evoprime/Athena/PROJ/evOS/stages/dojo/simple.xsm
-load --int=10 /home/evoprime/Athena/PROJ/evOS/stages/stage_11/halt_prog.xsm
-#load --int=timer /home/evoprime/Athena/PROJ/evOS/stages/stage_11/assign_timer.xsm
-load --exhandler /home/evoprime/Athena/PROJ/evOS/stages/stage_11/halt_prog.xsm
-load --int=7 /home/evoprime/Athena/PROJ/evOS/stages/stage_11/write_interrupt.xsm
+load --library /home/evoprime/PROJ/evOS/expl/library.lib
+load --os /home/evoprime/Athena/PROJ/evOS/stages/stage_12/assign_os_startup.xsm
+load --init /home/evoprime/Athena/PROJ/evOS/stages/stage_12/assign_num_to_100.xsm
+load --int=10 /home/evoprime/Athena/PROJ/evOS/stages/stage_12/halt_prog.xsm
+load --int=timer /home/evoprime/Athena/PROJ/evOS/stages/stage_12/timer.xsm
+load --exhandler /home/evoprime/Athena/PROJ/evOS/stages/stage_12/halt_prog.xsm
+load --int=7 /home/evoprime/Athena/PROJ/evOS/stages/stage_12/write_interrupt.xsm
+load --idle /home/evoprime/Athena/PROJ/evOS/stages/stage_12/assign_idle_process.xsm
