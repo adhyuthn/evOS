e21c9c8f800fd016c4c37f9a87ae361c1c56b160
adhyuthn <adhyuthnarayan008@gmail.com>
2024-09-06 01:45:04 +0530

    ✓ stage - 15 Resource Manager Module

--- xfs_inputs.sh
+++ xfs_inputs.sh
@@ -1,15 +1,15 @@
 fdisk
 # Stage 11 specifies to load lib before bootstrap. Not sure why.
 load --library /home/evoprime/PROJ/evOS/expl/library.lib
-load --os /home/evoprime/Athena/PROJ/evOS/stages/stage_14/os_startup.xsm
-load --init /home/evoprime/Athena/PROJ/evOS/stages/stage_14/odd_100.xsm
-load --int=timer /home/evoprime/Athena/PROJ/evOS/stages/stage_14/timer.xsm
-load --int=7 /home/evoprime/Athena/PROJ/evOS/stages/stage_14/write_interrupt.xsm
-load --int=10 /home/evoprime/Athena/PROJ/evOS/stages/stage_14/halt_prog.xsm
-load --exhandler /home/evoprime/Athena/PROJ/evOS/stages/stage_14/exhandler_prog.xsm
-load --module 7 /home/evoprime/Athena/PROJ/evOS/stages/stage_14/assign_mod7_boot_module.xsm
-load --module 5 /home/evoprime/Athena/PROJ/evOS/stages/stage_14/mod5_scheduler_module.xsm
-load --idle /home/evoprime/Athena/PROJ/evOS/stages/stage_14/idle_process.xsm
-load --exec /home/evoprime/Athena/PROJ/evOS/stages/stage_14/even_100.xsm
-load --exec /home/evoprime/Athena/PROJ/evOS/stages/dojo/simple.xsm
-
+load --os /home/evoprime/Athena/PROJ/evOS/stages/stage_15/os_startup.xsm
+load --init /home/evoprime/Athena/PROJ/evOS/stages/stage_15/odd_100.xsm
+load --int=timer /home/evoprime/Athena/PROJ/evOS/stages/stage_15/timer.xsm
+load --int=7 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/write_interrupt.xsm
+load --int=10 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/halt_prog.xsm
+load --exhandler /home/evoprime/Athena/PROJ/evOS/stages/stage_15/exhandler_prog.xsm
+load --module 7 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod7_boot_module.xsm
+load --module 5 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod5_scheduler_module.xsm
+load --module 0 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod0_resource_manager.xsm
+load --module 4 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod4_device_manager.xsm
+load --idle /home/evoprime/Athena/PROJ/evOS/stages/stage_15/idle_process.xsm
+load --exec /home/evoprime/Athena/PROJ/evOS/stages/stage_15/even_100.xsm
