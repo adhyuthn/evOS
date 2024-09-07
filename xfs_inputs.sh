43c1bd2453c49ef23a15c65facfdf5d2311e402d
adhyuthn <adhyuthnarayan008@gmail.com>
2024-09-07 15:55:27 +0530

    ✓ stage - 16 Console Input

--- xfs_inputs.sh
+++ xfs_inputs.sh
@@ -1,15 +1,16 @@
 fdisk
-# Stage 11 specifies to load lib before bootstrap. Not sure why.
-load --library /home/evoprime/PROJ/evOS/expl/library.lib
-load --os /home/evoprime/Athena/PROJ/evOS/stages/stage_15/os_startup.xsm
-load --init /home/evoprime/Athena/PROJ/evOS/stages/stage_15/odd_100.xsm
-load --int=timer /home/evoprime/Athena/PROJ/evOS/stages/stage_15/timer.xsm
-load --int=7 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/write_interrupt.xsm
-load --int=10 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/halt_prog.xsm
-load --exhandler /home/evoprime/Athena/PROJ/evOS/stages/stage_15/exhandler_prog.xsm
-load --module 7 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod7_boot_module.xsm
-load --module 5 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod5_scheduler_module.xsm
-load --module 0 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod0_resource_manager.xsm
-load --module 4 /home/evoprime/Athena/PROJ/evOS/stages/stage_15/mod4_device_manager.xsm
-load --idle /home/evoprime/Athena/PROJ/evOS/stages/stage_15/idle_process.xsm
-load --exec /home/evoprime/Athena/PROJ/evOS/stages/stage_15/even_100.xsm
+load --library      /home/evoprime/PROJ/evOS/expl/library.lib
+load --os           /home/evoprime/Athena/PROJ/evOS/stages/stage_16/os_startup.xsm
+load --init         /home/evoprime/Athena/PROJ/evOS/stages/stage_16/gcd.xsm
+load --int=timer    /home/evoprime/Athena/PROJ/evOS/stages/stage_16/timer_interrupt.xsm
+load --int=console  /home/evoprime/Athena/PROJ/evOS/stages/stage_16/console_interrupt.xsm
+load --int=6        /home/evoprime/Athena/PROJ/evOS/stages/stage_16/int6_read_interrupt.xsm
+load --int=7        /home/evoprime/Athena/PROJ/evOS/stages/stage_16/int7_write_interrupt.xsm
+load --int=10       /home/evoprime/Athena/PROJ/evOS/stages/stage_16/int10_halt_interrupt.xsm
+load --exhandler    /home/evoprime/Athena/PROJ/evOS/stages/stage_16/exhandler.xsm
+load --module 7     /home/evoprime/Athena/PROJ/evOS/stages/stage_16/mod7_boot_module.xsm
+load --module 5     /home/evoprime/Athena/PROJ/evOS/stages/stage_16/mod5_scheduler_module.xsm
+load --module 0     /home/evoprime/Athena/PROJ/evOS/stages/stage_16/mod0_resource_manager.xsm
+load --module 4     /home/evoprime/Athena/PROJ/evOS/stages/stage_16/mod4_device_manager.xsm
+load --idle         /home/evoprime/Athena/PROJ/evOS/stages/stage_16/idle_process.xsm
+#load --exec        /home/evoprime/Athena/PROJ/evOS/stages/stage_16/even_100.xsm
